485d03d9d8b1a252f01a334d169bcb3f76f73cb6
Laurix86 <grossoanalaura@gmail.com>
2023-09-03 23:46:27 -0300

    con test unitario

---
+++ app/src/androidTest/java/com/example/appticmas/comparar/view/MainActivityTest.kt
@@ -0,0 +1,20 @@
+package com.example.appticmas.comparar.view
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
