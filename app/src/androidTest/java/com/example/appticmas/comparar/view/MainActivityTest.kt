232ac20e6670d88f5a3560646e6a13754beaf2d2
Laurix86 <grossoanalaura@gmail.com>
2023-09-04 00:20:32 -0300

    archivo doc

--- app/src/androidTest/java/com/example/appticmas/comparar/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/appticmas/comparar/view/MainActivityTest.kt
@@ -1,22 +1,44 @@
 package com.example.appticmas.comparar.view
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.appticmas.R
 import org.junit.Assert.*
 
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    @get: Rule
     var rule : ActivityScenarioRule<*> = ActivityScenarioRule(MainActivity::class.java)
+
     @Before
     fun setUp() {
     }
 
     @After
     fun tearDown() {
+    }
+
+    @Test
+    fun mainActivity_compararStrings(){
+
+        onView(withId(R.id.inputFirstText)).perform(typeText("Aloha"))
+        onView(withId(R.id.inputSecondText)).perform(typeText("Aloha"))
+        onView(withId(R.id.compararButton)).perform(click())
+
     }
 }
